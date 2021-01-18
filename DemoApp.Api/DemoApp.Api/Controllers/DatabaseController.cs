f57187fd2458446c7a34a8f00b5f1609eb6d78ae
Rainer Stropek <rainer@software-architects.at>
2021-01-18 15:57:38 +0100

    Demo API

---
+++ DemoApp.Api/DemoApp.Api/Controllers/DatabaseController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public DatabaseController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<int> AccessDatabase()
+        {
+            using var connection = new SqlConnection(configuration["ConnectionStrings:DefaultConnection"]);
+            var token = await new AzureServiceTokenProvider()
+                .GetAccessTokenAsync("https://database.windows.net/", configuration["AADTenant"]);
+            connection.AccessToken = token;
+            await connection.OpenAsync();
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT 42 AS ANSWER";
+            return (int)await cmd.ExecuteScalarAsync();
+        }
+    }
+}
