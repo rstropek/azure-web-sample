5a67f11a6936ee0a0bbbc89340ff5a464ce4162a
Rainer Stropek <rainer@software-architects.at>
2021-01-19 08:59:28 +0100

    Add function call

--- DemoApp.Api/Controllers/AddController.cs
+++ DemoApp.Api/Controllers/AddController.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Identity;
+﻿using Azure.Core;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace DemoApp.Api.Controllers
@@ -25,12 +27,28 @@ namespace DemoApp.Api.Controllers
             this.configuration = configuration;
         }
 
+        record ApiResult(int A, int B, int Res);
+
         public async Task<IActionResult> AddNumbers()
         {
-            var client = new SecretClient(new Uri($"https://{configuration["KeyVault"]}.vault.azure.net/"), new DefaultAzureCredential());
-            var secret = client.GetSecret("apiKey");
-            await Task.Delay(1);
-            return Ok(secret.Value);
+            var token = await GetAccessToken("https://vault.azure.net/.default");
+
+            var kvClient = new SecretClient(new Uri($"https://{configuration["KeyVault"]}.vault.azure.net/"),
+                new DefaultAzureCredential(new DefaultAzureCredentialOptions { VisualStudioTenantId = configuration["AADTenant"] }));
+            var secret = kvClient.GetSecret("apiKey").Value;
+
+            var result = await client.GetFromJsonAsync<ApiResult>("https://"+ configuration["FunctionUrl"] + ".azurewebsites.net/api/Add?a=1&b=2&code=" + secret.Value);
+            return Ok(result);
+        }
+
+        async Task<string> GetAccessToken(string scope)
+        {
+            var tokenRequestResult = await new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                VisualStudioTenantId = configuration["AADTenant"]
+            }).GetTokenAsync(
+                new TokenRequestContext(new[] { scope }));
+            return tokenRequestResult.Token;
         }
     }
 }
