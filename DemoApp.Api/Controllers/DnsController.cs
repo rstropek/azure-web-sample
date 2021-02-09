e410cf81a30ba4d4cea52d23f2db51554978b033
Rainer Stropek <rainer@software-architects.at>
2021-02-09 16:57:29 +0100

    Add DNS controller

---
+++ DemoApp.Api/Controllers/DnsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace DemoApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DnsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public DnsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        record ApiResult(int A, int B, int Res);
+
+        public IActionResult GetIpAddresses()
+        {
+            var addressesString = new StringBuilder();
+            var addresses = Dns.GetHostAddresses($"{configuration["KeyVault"]}.vault.azure.net");
+            return Ok(string.Join(", ", addresses.ToArray().Select(x => x.ToString())));
+        }
+    }
+}
